6b9433c018a2f827c3b025304f87163ef676cc51
FrankPereira1B <frank.rithesh.pereira@gmail.com>
2025-10-26 20:08:28 +0000

    String Center manipulation

--- String_Method.py
+++ String_Method.py
@@ -1,4 +1,12 @@
+
+# Capitalising the first letter of a sentence
 def capitalize(txt):
     x = chr(ord(txt[0])-32)+ txt[1:]
     print(x)
     return x
+
+# String creates a string of x characters with the character in the center
+def center(txt, n):
+    x = abs(len(txt) -n)
+    txt = " "*x + txt + " "*x
+    print(txt)
---
+++ String_Test2.py
@@ -0,0 +1,4 @@
+from String_Method import center
+txt = "banana"
+n = 20
+center(txt, n)
