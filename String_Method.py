de19608d583ca482e3144fa5af4f52ac80a47a1f
FrankPereira1B <frank.rithesh.pereira@gmail.com>
2025-10-27 18:05:58 +0000

    Capitalise 2 method

--- String_Method.py
+++ String_Method.py
@@ -8,6 +8,31 @@ def capitalize(txt):
     print(x)
     return x
 
+''' Capitalize method without using any in built functions
+    txt: text given
+    chr_list = list of ascii characters for capital letters
+    ord_list = list of ascii characters for small letters'''
+
+def capitalize2(txt, chr_list, ord_list):
+    x = txt[0]
+    if x in ord_list:
+        y = ord_list.get(x)
+        z = y - 32
+        print(x)
+        print(y)
+        print(z)
+    else:
+        print("No ascii character")
+        
+    for key, val in chr_list.items():
+        if val == z:
+            cap = key
+            print(cap)
+            txt = cap+txt[1:]
+            print(txt)
+    return x, y, z, cap, txt
+
+
 # String creates a string of x characters with the character in the center
 ''' txt: Required here to pass through a string will be centred according to the length"
     n = Basically dictates length of a string'''
